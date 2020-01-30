fc9257477f7d73f50506d2907213084bc4b926cc
Adam Huang <adamhut@gmail.com>
2020-01-30 07:14:45 -0800

    wip

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,44 +1,59 @@
+console.log('it works');
+
 const cardButtons = document.querySelectorAll('.card button');
 const modalOuter = document.querySelector('.modal-outer');
 const modalInner = document.querySelector('.modal-inner');
 
-function handleCardButtonClick(event) {
-  const button = event.currentTarget;
-  const card = button.closest('.card');
-  // Grab the image src
-  const imgSrc = card.querySelector('img').src;
-  const desc = card.dataset.description;
-  const name = card.querySelector('h2').textContent;
-  // populate the modal with the new info
-  modalInner.innerHTML = `
-    <img width="600" height="600" src="${imgSrc.replace(
-      '200',
-      '600'
-    )}" alt="${name}"/>
-    <p>${desc}</p>
-  `;
-  // show the modal
-  modalOuter.classList.add('open');
+function handleCardButtonClick(event){
+    console.log('YA clicked it');
+    const button  = event.currentTarget;
+    const card = button.closest('.card');
+    // console.log(card);
+    const imgSrc = card.querySelector('img').src;
+    
+    const name = card.querySelector('h2').textContent;
+
+    console.log(imgSrc);
+
+    const desc = card.dataset.description;
+
+    console.log(desc);
+
+    //populate the modal with the new info
+    modalInner.innerHTML =`
+        <img width="600" height="600" src="${imgSrc.replace('200','600')} alt="${name}" />
+        <p>
+        ${desc}
+        </p>
+    `;
+
+    modalOuter.classList.add('open');
+
 }
 
-cardButtons.forEach(button =>
-  button.addEventListener('click', handleCardButtonClick)
-);
+cardButtons.forEach(button=> button.addEventListener('click',handleCardButtonClick));
 
-function closeModal() {
-  modalOuter.classList.remove('open');
+function closeModal(){
+    modalOuter.classList.remove('open');
 }
 
-modalOuter.addEventListener('click', function(event) {
-  const isOutside = !event.target.closest('.modal-inner');
-  if (isOutside) {
-    closeModal();
-  }
+
+modalOuter.addEventListener('click',function(event){
+    const isOutside = ! event.currentTarget.closest('.modal-innder');
+    
+    if(isOutside )
+    {
+        closeModal();
+    }
+    // console.log(event);
+
 });
 
-window.addEventListener('keydown', event => {
-  console.log(event);
-  if (event.key === 'Escape') {
-    closeModal();
-  }
+window.addEventListener('keydown',(event) => {
+    console.log(event.key);
+
+    if(event.key==='Escape')
+    {
+         closeModal();
+    }
 });
--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,58 @@
+function scrollToAccept(){
+    const terms = document.querySelector('.terms-and-conditions');
+    if(!terms)
+    {
+        return;//quit this
+    }
+
+    terms.addEventListener('scroll', function (e) {
+        // console.log(e);
+    });
+}
+
+// scrollToAccept();
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+
+const btn = document.querySelector('.accept');
+
+function obCallback(payload){
+
+    console.log(payload[0].isIntersecting , payload[0].intersectionRatio);
+
+    if (payload[0].intersectionRatio === 1)
+    {
+        btn.disabled = false;
+        console.log('REMOVING DISABLED')
+        ob.unobserve(terms.lastElementChild);
+    }else{
+        // btn.disabled = true;
+    }
+
+}
+
+const ob = new IntersectionObserver(obCallback,{
+    root:terms,
+    threshold: 1, 
+    //threshold: [0,0.1],
+});
+
+// ob.observe(watch);
+
+ob.observe(terms.lastElementChild);
+
+
+
+terms.addEventListener('scroll', function (e) {
+    
+    // console.log(e.currentTarget);
+    e.currentTarget.scrollTop 
+    e.currentTarget.scrollHeight;
+
+
+
+});
 console.log('IT WORKS!');
+
+
+
