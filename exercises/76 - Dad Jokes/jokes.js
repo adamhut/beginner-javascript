f5b3fed3dd7ffa4feacacf36b3e6f000a6f53638
Adam Huang <adamhut@gmail.com>
2020-03-04 06:43:31 -0800

    wip

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,8 @@
+const jokeButton = document.querySelector('.getJoke');;
+const jokeHolder = document.querySelector('.joke p');
+const jokeButtonSpan = jokeButton.querySelector('.jokeText');
+const loader = jokeButton.querySelector('.loader');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +13,51 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+
+async function fetchJoke(){
+
+
+    jokeButtonSpan.classList.add('hidden');
+    const response = await fetch("https://icanhazdadjoke.com",{
+        headers:{
+            Accept: 'application/json',
+            // Accept: 'text/plain',
+        }
+    });
+
+    const data = await response.json();
+    jokeButtonSpan.classList.remove('hidden');
+    return data
+    // console.log(joke);
+
+}
+
+function randomItemFromArray(arr, not){
+
+  const item = arr[(Math.floor(Math.random() * arr.length)) ];
+
+  if(item === not)
+  {
+      console.log('Ahh we used taht one last time, look again');
+      return randomItemFromArray(arr, not);
+  }
+
+  return item;
+
+}
+
+async function handleClick(){
+    loader.classList.remove('hidden');
+    const {joke}  = await fetchJoke();
+    // console.log(joke);
+    jokeHolder.textContent = joke;
+    jokeButtonSpan.textContent = randomItemFromArray(buttonText, jokeButtonSpan.textContent);
+    loader.classList.add('hidden');
+}
+
+jokeButton.addEventListener('click', handleClick);
+
+
+fetchJoke();
+
