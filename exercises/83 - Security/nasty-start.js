9a3db643d001a72f39b84ada54522b268225b459
Adam Huang <adamhut@gmail.com>
2020-03-10 07:08:08 -0700

    wip

---
+++ exercises/83 - Security/nasty-start.js
@@ -0,0 +1,24 @@
+import {sanitize} from 'dompurify';
+
+const input     = document.querySelector('[name=input]');
+const output    = document.querySelector('.output');
+const buttons   = document.querySelectorAll('nav button');
+
+input.addEventListener('input',()=>{
+    
+    const clean  = sanitize(input.value,{
+        FORBID_ATTR:['width','height','style'],
+        FORBID_TAGS:['style']
+    });
+
+    output.innerHTML = clean.replace(/\n/g, '<br>');
+})
+
+//trigger the event on page load
+input.dispatchEvent(new Event('input'));
+
+buttons.forEach(button=>{
+    button.addEventListener('click', e => {
+        alert(e.currentTarget.textContent);
+    });
+});
